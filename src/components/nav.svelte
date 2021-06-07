68e00f031757284e0e8bd4c09c17ec542bbacb15
Tarik Kavaz <tarikkavaz@gmail.com>
2021-06-07 05:40:30 +0300

    fade

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,5 +1,9 @@
+<script>
+	import { fade } from 'svelte/transition';
+</script>
+
 <nav class="flex justify-center w-full">
-	<a class="mx-4 text-lg" href="/">Home</a>
-	<a class="mx-4 text-lg" href="/about">About</a>
-	<a class="mx-4 text-lg" href="/api/pokemon">API</a>
+	<a class="mx-4 text-lg" transition:fade href="/">Home</a>
+	<a class="mx-4 text-lg" transition:fade href="/about">About</a>
+	<a class="mx-4 text-lg" transition:fade href="/api/pokemon">API</a>
 </nav>
